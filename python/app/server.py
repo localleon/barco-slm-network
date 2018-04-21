c4a67696d9c370d7f17b58d6aa5128759dd00aef
itsLeonR <leon.rauschenbach@web.de>
2018-04-21 19:24:32 +0200

    Cleaning up

--- python/app/server.py
+++ python/app/server.py
@@ -11,10 +11,12 @@ import logging
 from logging.handlers import RotatingFileHandler
 logger = logging.getLogger('werkzeug')  # Gets Flasks main Logger
 
-# from serial import *       # Methodes for all the Serial Stuff
-import serial
+# ============= SERIAL STuff
+import serial   # Importing pyserial Libary
+ser = serial.Serial('/dev/ttyUSB0')  # open serial port with baudrate of 9600 and standard settings
+
 def send_serial(tosend):
-    
+    # Sends @tosend to the serial port
     ser.write(tosend)
 
 #============  CONFIG  ============
@@ -22,7 +24,6 @@ from apiconfig import *        # Imports Config Values
 app = Flask(__name__)          # Inits Flask APP
 CORS(app)                      # Adds a Cors Header to the Responses
 auth = HTTPBasicAuth()         # HTTP Basic Authentication
-ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 # =================== Logging ===================================0
 
 def file_logger():
@@ -36,34 +37,17 @@ def file_logger():
                                   backupCount=2, encoding=None, delay=0)
     logger.addHandler(handler)
 
-
-def sessionlogger(request):
-    """
-    This function appends every request with some useful informations as a string to an array. 
-    The array is used as a temporary log. 
-    """
-    if(len(sessionlog) >= 100):
-        del sessionlog[0:10]
-    sessionlog.append(str('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + " : " +
-                          request.remote_addr + " " +
-                          request.method + " " +
-                          request.scheme + " " +
-                          request.full_path
-                          ))
-
 #============================= APP Routes for GUIS ===================================0
 @app.route('/help')
 @auth.login_required
 def send_help():
     # Displays Help/Documentary Site
-    sessionlogger(request)
     return render_template('help.html'), 200
 
 @app.route('/')
 @auth.login_required
 def send_app():
     # Returns the Frontend
-    sessionlogger(request)
     return render_template('app.html'), 200
 
 #================= API Function Route ===================================
@@ -72,9 +56,8 @@ def send_app():
 @app.route('/app/send/<string:cmd>', methods=['GET', 'POST', 'PUT'])
 @auth.login_required
 def tasks_control(cmd):
-    sessionlogger(request)
+    # Matches Api Keys in serial_commands with gotten cmd and sending it out to serial port
     if cmd in serial_commands.keys():
-        logger.info(serial_commands[cmd])
         send_serial(serial_commands[cmd])
         return jsonify(success_response), 200
     else:
@@ -92,33 +75,26 @@ def ping():
     """
     return Response("pong", mimetype='text/plain')
 
-#=============00 AUTHENTICATION ============================
-
-
+#=============HTTP Basic  AUTHENTICATION ============================
 @auth.get_password
 def get_pw(username):
     if username in users:
         return users[username]
     return None
 
-
-#--------------Redirect on Port 80
+#================ Redirect on Port 80 ==================
 redirectApp = Flask("redirectApp")
 # redirect http traffic to https:
-
 
 @redirectApp.before_request
 def before_request():
     if request.url.startswith('http://'):
         return redirect(request.url.replace('http://', 'https://', 1), code=301)
 
-
 def start_redirect():
     redirectApp.run(port=80, host="0.0.0.0")
 
 #==================== MAIN =================================
-
-
 if __name__ == '__main__':
     # file_logger()
 
