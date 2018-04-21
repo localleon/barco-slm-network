054da559767f46761478aca88492da5fcb93971c
unknown <hundemeier99@gmail.com>
2018-04-21 20:17:56 +0200

    added go test

---
+++ go/test.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "log"
+import "github.com/jacobsa/go-serial/serial"
+
+func main() {
+
+	// Set up options.
+	options := serial.OpenOptions{
+		PortName:        "COM4",
+		BaudRate:        9600,
+		DataBits:        8,
+		StopBits:        1,
+		MinimumReadSize: 4,
+	}
+
+	// Open the port.
+	port, err := serial.Open(options)
+	if err != nil {
+		log.Fatalf("serial.Open: %v", err)
+	}
+
+	// Make sure to close it later.
+	defer port.Close()
+
+	b := []byte{0xfe, 0x01, 0x22, 0x42, 0x00, 0x65, 0xff}
+	//b := []byte{0xfe, 0x01, 0x23, 0x42, 0x00, 0x66, 0xff}
+	n, err := port.Write(b)
+	if err != nil {
+		log.Fatalf("port.Write: %v", err)
+	}
+
+	fmt.Println("Wrote", n, "bytes.")
+
+}
