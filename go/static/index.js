326217a68cdd9cf5c9696bdfac73d459f246f463
localleon <me@lrau.systems>
2018-04-25 11:28:19 +0200

    Frontend can now send apirequests

--- go/static/index.js
+++ go/static/index.js
@@ -1,1 +1,17 @@
-console.log("Welcome")
+window.onload = function (){
+    console.log('App started')
+}
+
+function apirequest(cmd, opt){
+    if(opt == ''){
+        fetch('/api' + '/' +  cmd , {
+            method : 'GET',
+            credentials : 'same-origin'
+        })     
+    }else{
+        fetch('/api' + '/' +  cmd + '/' + opt, {
+            method : 'GET',
+            credentials : 'same-origin'
+        })
+    }
+}
