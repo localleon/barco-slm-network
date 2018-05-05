89b85119b3d6a22a1a2aa35f7bdedac70f006e3f
itsLeonR <leon.rauschenbach@web.de>
2018-05-05 11:58:15 +0200

    Implemented readLCD to Frontend, needs to be tested

--- go/static/index.js
+++ go/static/index.js
@@ -1,17 +1,32 @@
-window.onload = function (){
+window.onload = function () {
     console.log('App started')
 }
 
-function apirequest(cmd, opt){
-    if(opt == ''){
-        fetch('/api' + '/' +  cmd , {
-            method : 'GET',
-            credentials : 'same-origin'
-        })     
-    }else{
-        fetch('/api' + '/' +  cmd + '/' + opt, {
-            method : 'GET',
-            credentials : 'same-origin'
-        })
+function apirequest(cmd, opt) {
+    // Standard Request to the Backend
+    var url;
+    if (opt == '') {
+        url = '/api/' + cmd;
+    } else {
+        url = '/api/' + cmd + '/' + opt;
     }
+    fetch(url, {
+        method: 'GET',
+        credentials: 'same-origin',
+    })
+}
+
+function readLCD() {
+    fetch('/api/readlcd')
+        .then(function (response) {
+            parseLCD(response.json())
+        })
+}
+
+function parseLCD(json) {
+    // Parse json Response and write to html element
+    var lcdfirst = document.getElementById("lcdfirst")
+    var lcdsecond = document.getElementById("lcdsecond")
+    lcdfirst.innerHTML = json.first
+    lcdsecond.innerHTML = json.second
 }
